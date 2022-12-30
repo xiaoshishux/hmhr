a829b48777698c331116a67aa31f49fa73880088
肖遥清 <1334823022@qq.com>
2022-12-30 20:01:43 +0800

    人资项目初始化_肃清模板后

---
+++ src/permission.js
@@ -0,0 +1,12 @@
+import router from './router'
+
+// const whiteList = ['/login'] // no redirect whitelist
+
+router.beforeEach(async(to, from, next) => {
+
+  next()
+})
+
+router.afterEach(() => {
+
+})
