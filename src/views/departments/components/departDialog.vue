89b076c5b016338284fc072948058f060bc519c7
肖遥清 <1334823022@qq.com>
2023-01-09 23:27:58 +0800

    组织架构-添加与编辑-表单验证

--- src/views/departments/components/departDialog.vue
+++ src/views/departments/components/departDialog.vue
@@ -8,22 +8,22 @@
       :close-on-presss-escape="false"
       :show-close="false"
     >
-      <el-form ref="deptForm" label-width="120px">
-        <el-form-item label="部门名称">
+      <el-form ref="deptForm" :model="form" :rules="rules" label-width="120px">
+        <el-form-item label="部门名称" prop="name">
           <el-input
             v-model="form.name"
             style="width: 80%"
             placeholder="1-50个字符"
           />
         </el-form-item>
-        <el-form-item label="部门编码">
+        <el-form-item label="部门编码" prop="code">
           <el-input
             v-model="form.code"
             style="width: 80%"
             placeholder="1-50个字符"
           />
         </el-form-item>
-        <el-form-item label="部门负责人">
+        <el-form-item label="部门负责人" prop="manager">
           <el-select
             v-model="form.manager"
             style="width: 80%"
@@ -36,7 +36,7 @@
             />
           </el-select>
         </el-form-item>
-        <el-form-item label="部门介绍">
+        <el-form-item label="部门介绍" prop="introduce">
           <el-input
             v-model="form.introduce"
             style="width: 80%"
@@ -79,6 +79,38 @@ export default {
         manager: "", // 部门管理者
         introduce: "", // 部门介绍
       },
+      rules: {
+        name: [
+          { required: true, message: "部门名称不能为空", trigger: "blur" },
+          {
+            min: 1,
+            max: 50,
+            message: "部门名称要求1-50个字符",
+            trigger: "blur",
+          },
+        ],
+        code: [
+          { required: true, message: "部门编码不能为空", trigger: "blur" },
+          {
+            min: 1,
+            max: 50,
+            message: "部门编码要求1-50个字符",
+            trigger: "blur",
+          },
+        ],
+        manager: [
+          { required: true, message: "部门负责人不能为空", trigger: "blur" },
+        ],
+        introduce: [
+          { required: true, message: "部门介绍不能为空", trigger: "blur" },
+          {
+            min: 1,
+            max: 300,
+            message: "部门介绍要求1-300个字符",
+            trigger: "blur",
+          },
+        ],
+      },
     };
   },
   computed: {},
@@ -92,15 +124,19 @@ export default {
     },
     // 确定-点击事件
     enterFn() {
-      this.$emit("addDepartEV", this.form);
-      this.form= {
-        name: "", // 部门名称
-        code: "", // 部门编码
-        manager: "", // 部门管理者
-        introduce: "", // 部门介绍
-      }
-      this.$refs.deptForm.resetFields()
-      this.$emit("update:dialogVisible", false);
+      this.$refs.deptForm.validate(async (valid) => {
+        if (valid) {
+          this.$emit("addDepartEV", this.form);
+          this.form = {
+            name: "", // 部门名称
+            code: "", // 部门编码
+            manager: "", // 部门管理者
+            introduce: "", // 部门介绍
+          };
+          this.$refs.deptForm.resetFields();
+          this.$emit("update:dialogVisible", false);
+        }
+      });
     },
   },
 };
