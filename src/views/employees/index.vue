e0ad56625056ca1e05c70ce37137b24182e79266
肖遥清 <1334823022@qq.com>
2023-01-15 16:47:19 +0800

    员工管理-头部组件-封装使用

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,13 +1,33 @@
 <template>
-    <div class="employees-container">
-      <div class="app-container">
-        <h2>员工</h2>
-      </div>
+  <div class="employees-container">
+    <div class="app-container">
+      <!-- 通用工具栏组件使用 -->
+      <page-tools>
+        <!-- 自定义左侧内容 -->
+        <template #slot-left>
+          <span>共 19 条记录</span>
+        </template>
+
+        <!-- 自定义右侧内容 -->
+        <template #slot-right>
+          <el-button type="danger" size="small">导入excel</el-button>
+          <el-button type="success" size="small">导出excel</el-button>
+          <el-button type="primary" size="small">新增员工</el-button>
+        </template>
+      </page-tools>
     </div>
-  </template>
-  
-  <script>
-  export default {}
-  </script>
-  
-  <style lang="scss" scoped></style>
+  </div>
+</template>
+
+<script>
+// 导入组件
+import PageTools from '@/components/PageTools'
+
+export default {
+  components: {
+    PageTools // 导入自定义组件
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
